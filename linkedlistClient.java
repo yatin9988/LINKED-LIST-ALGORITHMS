343c292569fa5b5128938e8e3894afa469fa1b68
YATIN <yatin88617@gmail.com>
2019-08-22 09:12:53 +0530

    client class

--- linkedlistClient.java
+++ linkedlistClient.java
@@ -2,9 +2,15 @@ package LinkedList;
 
 public class linkedlistClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+        linkedlist list = new linkedlist();
+        list.addAt(0,0);
+        list.addLast(10);
+        list.addFirst(20);
+//        list.display();
+        list.addAt(4,11);
+        list.display();
 	}
 
 }
